db84c3997980d15518051fc9e50136480451d15e
Berrydoo <jim.dewberry@gmail.com>
2020-06-10 17:16:41 -0400

    Custom Button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, ...otherProps }) => {
-    console.log(children);
-    return (
-        <button className="custom-button" {...otherProps}>
-            {children}
-        </button>
-    );
-};
+const CustomButton = ({ children, ...otherProps }) => (
+    <button className="custom-button" {...otherProps}>
+        {children}
+    </button>
+);
 
 export default CustomButton;

1b23255d4ab71d758a11f9d42e2ca4e81757ff2a
Berrydoo <jim.dewberry@gmail.com>
2020-06-27 18:29:55 -0400

    added signin redirect, and user action types

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header/header.component";
@@ -43,15 +43,29 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route exact path="/shop" component={ShopPage} />
-                    <Route exact path="/signIn" component={SignInSignUpPage} />
+                    <Route
+                        exact
+                        path="/signIn"
+                        render={() =>
+                            this.props.currentUser ? (
+                                <Redirect to="/" />
+                            ) : (
+                                <SignInSignUpPage />
+                            )
+                        }
+                    />
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);

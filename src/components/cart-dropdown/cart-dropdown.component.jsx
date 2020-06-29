3152dee0140178d9bd8fcba8a845da9a51ac4844
Berrydoo <jim.dewberry@gmail.com>
2020-06-29 09:09:20 -0400

    Cart Item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ isHidden, cartItems }) =>
     !isHidden && (
         <div className="cart-dropdown">
-            {cartItems.map((item) => (
-                <div className="cart-items" key={item.id}>
-                    <div>
-                        {item.name} ({item.qty})
-                    </div>
-                    <div>{item.price}</div>
-                </div>
-            ))}
-
+            <div className="cart-items">
+                {cartItems.length === 0
+                    ? "Please add something to your cart"
+                    : cartItems.map((item) => (
+                          <CartItem key={item.id} item={item} />
+                      ))}
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     );
 
-const mapStateToProps = (state) => ({
-    isHidden: state.cart.isHidden,
-    cartItems: state.cart.cartItems,
+const mapStateToProps = ({ cart }) => ({
+    isHidden: cart.isHidden,
+    cartItems: cart.cartItems,
 });
 
 export default connect(mapStateToProps)(CartDropdown);

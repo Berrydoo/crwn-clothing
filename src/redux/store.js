a3fc4b2c413760cd044c03bff9ac5936bba53e46
Berrydoo <jim.dewberry@gmail.com>
2020-07-01 10:41:41 -0400

    persist cart in local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
     rootReducer,
     applyMiddleware(...middlewares) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+//export default { store, persistor };

fda435eed4d916e3cf2dc34f6f42731b1758b4e6
Berrydoo <jim.dewberry@gmail.com>
2020-07-07 11:53:28 -0400

    convert collection data from an array to an object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,13 +5,22 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-//import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPage = ({ match }) => (
-    <div className="collection-page">
-        <h2>Collection page</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+
+    return (
+        <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+                {items.map((item) => (
+                    <CollectionItem item={item} key={item.id} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state),
